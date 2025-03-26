bcb16131c18f48858cade5d3d9d6b794dff4d155
Mistoruu <maille.eliott@gmail.com>
2025-03-26 10:35:09 +0100

    Init commit

---
+++ E6-mobile/Class1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E6_mobile
+{
+    internal class FileName
+    {
+        public static void Main(string[] args)
+        {
+            StreamReader sr;
+            Console.WriteLine("Hello World");
+        }
+    }
+}
---
+++ E6-mobile/Form1.cs
@@ -0,0 +1,10 @@
+namespace E6_Mobile
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
