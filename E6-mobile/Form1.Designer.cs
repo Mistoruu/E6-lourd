67b1a76e30468a5848e266f67f8c3756735aa99f
Mistoruu <maille.eliott@gmail.com>
2025-04-01 23:10:02 +0200

    maj

--- E6-mobile/Form1.Designer.cs
+++ E6-mobile/Form1.Designer.cs
@@ -28,12 +28,145 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            panel1 = new Panel();
+            panel2 = new Panel();
+            panel4 = new Panel();
+            panel3 = new Panel();
+            label1 = new Label();
+            label2 = new Label();
+            button1 = new Button();
+            label3 = new Label();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            panel2.SuspendLayout();
+            SuspendLayout();
+            // 
+            // panel1
+            // 
+            panel1.Location = new Point(12, 0);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(60, 453);
+            panel1.TabIndex = 0;
+            panel1.Paint += panel1_Paint;
+            // 
+            // panel2
+            // 
+            panel2.BackColor = Color.MidnightBlue;
+            panel2.Controls.Add(panel1);
+            panel2.Controls.Add(panel4);
+            panel2.Location = new Point(0, 0);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(801, 64);
+            panel2.TabIndex = 1;
+            // 
+            // panel4
+            // 
+            panel4.BackColor = Color.MidnightBlue;
+            panel4.Location = new Point(741, 0);
+            panel4.Name = "panel4";
+            panel4.Size = new Size(60, 453);
+            panel4.TabIndex = 1;
+            // 
+            // panel3
+            // 
+            panel3.BackColor = Color.MidnightBlue;
+            panel3.Location = new Point(0, 386);
+            panel3.Name = "panel3";
+            panel3.Size = new Size(801, 64);
+            panel3.TabIndex = 2;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Arial", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label1.Location = new Point(101, 127);
+            label1.Name = "label1";
+            label1.Size = new Size(99, 23);
+            label1.TabIndex = 3;
+            label1.Text = "Username";
+            label1.Click += label1_Click;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Arial", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label2.Location = new Point(101, 203);
+            label2.Name = "label2";
+            label2.Size = new Size(98, 23);
+            label2.TabIndex = 4;
+            label2.Text = "Password";
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.MidnightBlue;
+            button1.ForeColor = SystemColors.ButtonHighlight;
+            button1.Location = new Point(471, 295);
+            button1.Name = "button1";
+            button1.Size = new Size(126, 44);
+            button1.TabIndex = 5;
+            button1.Text = "Se connecter";
+            button1.UseVisualStyleBackColor = false;
+            button1.Click += button1_Click_1;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Arial", 10.2F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label3.ForeColor = Color.Red;
+            label3.Location = new Point(101, 295);
+            label3.Name = "label3";
+            label3.Size = new Size(168, 19);
+            label3.TabIndex = 6;
+            label3.Text = "Mot de passe oublié ?";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(369, 127);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(228, 27);
+            textBox1.TabIndex = 7;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(369, 203);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(228, 27);
+            textBox2.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.Window;
+            ClientSize = new Size(735, 450);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Controls.Add(label3);
+            Controls.Add(button1);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(panel3);
+            Controls.Add(panel2);
+            FormBorderStyle = FormBorderStyle.None;
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            panel2.ResumeLayout(false);
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Panel panel1;
+        private Panel panel2;
+        private Panel panel4;
+        private Panel panel3;
+        private Label label1;
+        private Label label2;
+        private Button button1;
+        private Label label3;
+        private TextBox textBox1;
+        private TextBox textBox2;
     }
 }
--- E6-mobile/Form1.cs
+++ E6-mobile/Form1.cs
@@ -1,10 +1,112 @@
+using System;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+using BCrypt.Net;
+using System.Linq.Expressions;
+
 namespace E6_Mobile
 {
     public partial class Form1 : Form
     {
+        private MySqlConnection conn;
+
         public Form1()
         {
             InitializeComponent();
         }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            string email = textBox1.Text;
+            string password = textBox2.Text;
+
+            if (string.IsNullOrWhiteSpace(email) || string.IsNullOrWhiteSpace(password))
+            {
+                MessageBox.Show("Veuillez remplir toutes les informations");
+                return;
+            }
+
+            string servername = "localhost";
+            string dbname = "bdd_e6_desktop";
+            string dbusername = "root";
+            string dbpassword = "";
+
+            string connString = $"Server={servername};Database={dbname};Uid={dbusername};Pwd={dbpassword};";
+
+            try
+            {
+                conn = new MySqlConnection(connString);
+                conn.Open();
+
+                string query = "SELECT * FROM users WHERE email = @Email";
+                MySqlCommand cmd = new MySqlCommand(query, conn);
+                cmd.Parameters.AddWithValue("@Email", email);
+                MySqlDataReader reader = cmd.ExecuteReader();
+
+                if (reader.Read())
+                {
+                    string storedPassword = reader["password"].ToString();
+                    if (BCrypt.Net.BCrypt.Verify(password, storedPassword))
+                    {
+                        MessageBox.Show("Connexion réussie");
+                        Form2 form2 = new Form2();
+                        form2.Show();
+                        this.Hide();
+                    }
+                    else
+                    {
+                        MessageBox.Show("Mot de passe incorrect");
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Utilisateur non trouvé");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Erreur de connexion : " + ex.Message);
+            }
+            finally
+            {
+                if (conn != null && conn.State == System.Data.ConnectionState.Open)
+                {
+                    conn.Close();
+                }
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
     }
+
 }
